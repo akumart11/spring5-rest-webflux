e970e68012371e315d27dbccb45ddcf479efa5a8
Avinash <43861508+akumart11@users.noreply.github.com>
2020-04-29 03:32:46 +0530

    Update Spring5RestWebfluxApplicationTests.java

--- src/test/java/avinash/springframework/spring5restwebflux/Spring5RestWebfluxApplicationTests.java
+++ src/test/java/avinash/springframework/spring5restwebflux/Spring5RestWebfluxApplicationTests.java
@@ -1,13 +1,16 @@
 package avinash.springframework.spring5restwebflux;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class Spring5RestWebfluxApplicationTests {
+public class Spring5WebfluxRestApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
