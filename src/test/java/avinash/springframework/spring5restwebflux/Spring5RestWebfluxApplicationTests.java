c2c14d77e6068997e7d33d9b1549b95e4c00126d
Avinash <akumart11@gmail.com>
2020-04-29 03:26:56 +0530

    Added Customer Domain and Repo. Configured Mongo DB

--- src/test/java/avinash/springframework/spring5restwebflux/Spring5RestWebfluxApplicationTests.java
+++ src/test/java/avinash/springframework/spring5restwebflux/Spring5RestWebfluxApplicationTests.java
@@ -1,6 +1,6 @@
 package avinash.springframework.spring5restwebflux;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
